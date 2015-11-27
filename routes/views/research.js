ed17850c8aaa7f5a7ac6ec7ad16b19293acdbbf1
qiuzihe <htszdm@hotmail.com>
2015-11-27 15:35:23 +0800

    20151127

--- routes/views/research.js
+++ routes/views/research.js
@@ -3,6 +3,23 @@ var keystone = require('keystone');
 exports = module.exports = function(req, res) {
     
     var view = new keystone.View(req, res);
+    locals = res.locals;
+    
+    	// Load all categories
+	view.on('init', function(next) {
+		
+		keystone.list('Research').model.find({}).where('state', 'published').populate('images').sort('rank').exec(function(err, results) {
+			
+			if (err) {
+				console.log("not find research ");
+				return next(err);
+			}
+	
+			locals.researches = results;
+			next();
+		});
+		
+	});
     
     view.render('research');
     
