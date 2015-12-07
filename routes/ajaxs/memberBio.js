b6886463f1d26fb1bf73235fc4f1489ecb0a91f1
allenlverson <allenlverson333@sina.com>
2015-12-07 22:01:29 +0800

    尝试实现成员个人简介的ajax加载

---
+++ routes/ajaxs/memberBio.js
@@ -0,0 +1,13 @@
+var _ = require('underscore');
+var keystone = require('keystone');
+//var member= keystone.list('member');
+
+//exports = module.exports = function (req, res) {
+//	member.model.findOne({ name: req.params.name }).exec(function (err,member)
+//	$('a.neo-member').click(function(){
+//		var id=$(this).data('member');
+//		$.getJson('/member/'+id,function(data){
+//			var memberDiv=$('#memberModal');
+//			memberDiv.find('[name='name']').text(data.name)
+//		}
+//	}
