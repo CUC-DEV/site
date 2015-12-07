018a50605c047c93837dbc8def4f9b42fae37d40
Fusizhe <Fusizhe@hotmail.com>
2015-12-07 21:29:42 +0800

    feature(新闻):1.完成了左边栏的动态载入 2.做了一些页面的修改，变量和文件名称的改动

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,7 @@ exports = module.exports = function(app) {
     app.get('/', routes.views.home);
 
     app.get('/posts', routes.views.posts);
+    app.get('/posts/:title',routes.views.post)
     app.get('/researches', routes.views.researches);
     app.get('/members', routes.views.members);
     app.get('/resources', routes.views.resources);
