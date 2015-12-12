dcb5178ada6f014e08d02fffe820a9c29a9b7b72
Mengyu LIU <mengyu.dev@outlook.com>
2015-12-12 22:41:01 +0100

    refacto: add model TextDate to save all text data on pages instead of saving in posts with different type

---
+++ models/TextData.js
@@ -0,0 +1,34 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * TextData Model
+ * ===========
+ */
+
+var TextData = new keystone.List('TextData', {
+	track: true,
+	defaultSort: '-createdAt',
+    map: { name: 'title' }
+});
+
+TextData.add({
+	title: { type: String, required: true, initial:true},
+	note:{type:String,  label:"备注"},
+	technicalKey:{type:String,  label:"技术参数"},
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true, required:true, initial:true },
+	content: {
+		brief: { type: Types.Text},
+		detail: { type: Types.Html, wysiwyg: true}
+	}
+});
+
+
+/**
+ * Registration
+ * ============
+ */
+
+TextData.defaultSort = '-publishDate';
+TextData.defaultColumns = 'title, state|20%';
+TextData.register();
