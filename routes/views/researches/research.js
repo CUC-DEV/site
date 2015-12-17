b287b351bcd20f9b21e1b7b021ea0b6fd8b1b89c
qiuzihe <htszdm@hotmail.com>
2015-12-17 21:52:15 +0800

    修改了研究成果详情界面，可以显示leader和members成员名称，修改了post的index页面，使index可以显示所有新闻

--- routes/views/researches/research.js
+++ routes/views/researches/research.js
@@ -22,6 +22,40 @@ exports = module.exports = function(req, res) {
         });
 
     });
+    view.on('init', function(next) {
+        locals.leader= [];
+        keystone.list('Research').model.findOne({_id: id}).populate('leader').exec(function(err, leader) {
+            if (err) {
+                console.log("leader");
+                return next(err);
+            }
+
+            if(leader){
+                locals.leader.push(leader)
+            }
+
+            next();
+        });
+
+    });
+    view.on('init', function(next) {
+        locals.authors= [];
+        keystone.list('Research').model.findOne({_id: id}).populate('authors').exec(function(err, authors) {
+            if (err) {
+                console.log("not find authors ");
+                return next(err);
+            }
+
+            if(authors){
+                locals.authors.push(authors)
+                //console.log(locals.authors[0].authors[0].name);
+            }
+
+            next();
+        });
+
+    });    
+    
 
 	view.render('researchdetail');
 
