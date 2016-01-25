a946243f1e0c1e783b34cc5274b16706ea6c27ca
Fusizhe <Fusizhe@hotmail.com>
2016-01-25 23:26:18 +0800

    feature(论文页)： 1.添加了paper页的model 2.重写了路由 3.尝试修改了templates的jade文件，但依然存在error

---
+++ models/paper.js
@@ -0,0 +1,55 @@
+// Load .env for development environments
+require('dotenv').load();
+var data_path = process.env.DATA_PATH || (__dirname + '/data');
+
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Paper Model
+ * ===========
+ */
+
+var Paper = new keystone.List('Paper', {
+	track: true,
+	defaultSort: '-createdAt',
+    label: '论文',
+    map:{name:'title'}
+});
+
+Paper.add({
+	title: { type: String, required: true, initial:true,label:"题目"},
+    createdAt:{ type: Date, default: Date.now,label:"创建时间"},
+	author: {type: String,label:"作者"},
+	content: {type: Types.Html, wysiwyg: true,label:"内容"},
+	file: {
+		type: Types.LocalFile,
+		dest: data_path + '/achievement',
+		prefix: '/files/',
+		filename: function (item, file) {
+			return item.id + '.' + file.extension
+		},label:"上传文件"
+	},
+});
+
+/**
+ * Virtuals
+ * ========
+ */
+
+Paper.schema.virtual('url').get(function() {
+	return "/paper/"+this.id;
+});
+
+Paper.schema.virtual('name').get(function() {
+    return this.title;
+});
+
+
+
+/**
+ * Registration
+ * ============
+ */
+
+Paper.register();
--- routes/views/paper.js
+++ routes/views/paper.js
@@ -1,29 +1,13 @@
-/*var keystone = require('keystone');
+var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-    
-    var view = new keystone.View(req, res);
-			locals=res.locals;
-			
-	//load categories
-	
-	/*view.on('init', function(next) {
-		keystone.list('ResearchCategory').model.find({key:"paper"},{'_id':1}).exec(function(err, results) {
-			
-			if (err) {
-				console.log("not find category ");
-				return next(err);
-			}
-	
-			locals._id= results;
-			next();
-		});
-	});*/
-			
+
+	var view = new keystone.View(req, res),
+			locals = res.locals;
 	//load paper
-	/*view.on('init',function(next){
+	view.on('init',function(next){
 		
-		keystone.list('Research').model.find({}).exec(function(err,results){
+		keystone.list('Paper').model.find({}).exec(function(err,results){
 			
 			if(err){
 				console.log("paper not found");
@@ -39,9 +23,9 @@ exports = module.exports = function(req, res) {
     
     view.render('paper');
     
-}*/
+}
 
-var keystone = require('keystone');
+/*var keystone = require('keystone');
 
 exports = module.exports = function(req,res){
 	
@@ -49,4 +33,4 @@ exports = module.exports = function(req,res){
 	
 	view.render('paper');
 	
-}
+}*/
